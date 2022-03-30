54f393a9f2b3e667c29e599b9fb474daae4ec18c
Justas Balcas <juztas@gmail.com>
2022-03-29 18:16:12 -0700

    Node Exporter new release with exporting mac/ip info

---
+++ node-exporter/start-docker.sh
@@ -0,0 +1,10 @@
+docker run -d -p 9100:9100 \
+  --net="host" \
+  --pid="host" \
+  -v "/proc:/host/proc:ro" \
+  -v "/sys:/host/sys:ro" \
+  -v "/:/host:ro,rslave" \
+  --log-opt max-size=10m --log-opt max-file=10 \
+  node_exporter \
+  --path.rootfs=/host \
+  --collector.netdev.address-info
