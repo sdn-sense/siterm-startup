a31237ce566c43135730d96e57ee86269210d8e5
Justas Balcas <juztas@gmail.com>
2022-05-02 17:56:13 -0700

    Run ansible playbooks only if ansible_network_os is defined

--- fe/conf/opt/siterm/config/ansible/sense/test-runner.py
+++ fe/conf/opt/siterm/config/ansible/sense/test-runner.py
@@ -19,7 +19,7 @@ import ansible_runner
 def runAnsible(playbookFile):
     """Run Ansible Playbook"""
     ansRunner = ansible_runner.run(private_data_dir='/opt/siterm/config/ansible/sense',
-                                   inventory='/opt/siterm/config/ansible/inventory/inventory.yaml',
+                                   inventory='/opt/siterm/config/ansible/sense/inventory/inventory.yaml',
                                    playbook=playbookFile)
     return ansRunner
 
