e2557f9274bab1acd420c3288f7dc09a1b074aeb
Justas Balcas <juztas@gmail.com>
2022-08-01 11:37:00 -0700

    Enforce to use json-file log driver for node_exporter

--- node-exporter/docker/start-docker.sh
+++ node-exporter/docker/start-docker.sh
@@ -4,6 +4,7 @@ docker run -d -p 9100:9100 \
   -v "/proc:/host/proc:ro" \
   -v "/sys:/host/sys:ro" \
   -v "/:/host:ro,rslave" \
+  --log-driver "json-file" \
   --log-opt max-size=10m --log-opt max-file=10 \
   prom/node-exporter \
   --path.rootfs=/host \
