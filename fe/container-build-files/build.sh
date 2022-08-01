25133797c77f97a7ffafd04577484fe110373c25
Justas Balcas <juztas@gmail.com>
2022-08-01 09:32:26 -0700

    ppc64le Rust install in Base Img

--- fe/container-build-files/build.sh
+++ fe/container-build-files/build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-docker build --no-cache -t sitefe-main .
+RELEASE=dev
+if [ $# -eq 1 ]
+  then
+    echo "Argument specified. Will use $1 to tag docker image"
+    RELEASE=$1
+fi
+
+ARCH=`arch`
+if [ $ARCH = "x86_64" ]; then
+  docker build --no-cache -t sitefe --build-arg RELEASE=$RELEASE .
+elif [ $ARCH = "ppc64le" ]; then
+  docker build --no-cache -t sitefe --build-arg RELEASE=$RELEASE"-"$ARCH .
+fi
