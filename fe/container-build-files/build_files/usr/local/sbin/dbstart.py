d551f70d1400efb5171cea7993125fa76d085c50
Justas Balcas <juztas@gmail.com>
2025-01-02 14:12:11 -0600

    DB Update fixes

--- fe/container-build-files/build_files/usr/local/sbin/dbstart.py
+++ fe/container-build-files/build_files/usr/local/sbin/dbstart.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Database starter to create and update database"""
 import pkg_resources
+import mariadb
 from SiteRMLibs.DBBackend import dbinterface
 from SiteRMLibs import __version__ as runningVersion
 from SiteRMLibs.GitConfig import getGitConfig
@@ -12,22 +13,43 @@ class DBStarter:
         self.config = getGitConfig()
         self.db = dbinterface('DBINIT', self.config, "MAIN")
 
+    @staticmethod
+    def _getversionfloat(valin):
+        valspl = valin.split('.')
+        if len(valspl) == 1:
+            return float(valspl[0])
+        intpart = valspl[0]
+        fracpart = ''.join(valspl[1:])
+        return float(f"{intpart}.{fracpart}")
+
+    def _makesqlcall(self, sqlcall):
+        try:
+            self.db.db.execute(sqlcall)
+        except mariadb.ProgrammingError as ex:
+            print(f"Error executing SQL: {ex}")
+            raise
+        except mariadb.OperationalError as ex:
+            print(f"Error executing SQL: {ex}")
+            raise
+
+
     def _getversion(self):
         version = self.db.get("dbversion", limit=1)
         # If no version is found, write the current version
         if not version:
-            self.db.insert("dbversion", [{"version": runningVersion}])
-            return runningVersion
-        return version[0]["version"]
+            vval = self._getversionfloat(runningVersion)
+            self.db.insert("dbversion", [{"version": vval}])
+            return vval
+        return self._getversionfloat(version[0]["version"])
 
     def _getAllModFiles(self):
         """Get all the modification files"""
-        moddir = pkg_resources.resource_listdir('SiteFE', 'packaging/release_mods')
+        moddir = pkg_resources.resource_listdir('SiteFE', 'release/release_mods')
         modfiles = {}
         for mod in moddir:
-            modv = mod.replace(".", "")
+            modv = self._getversionfloat(mod)
             modfiles.setdefault(modv, {'dirname': mod, 'files': []})
-            modfiles[modv]['files'] = pkg_resources.resource_listdir('SiteFE', f'packaging/release_mods/{mod}')
+            modfiles[modv]['files'] = pkg_resources.resource_listdir('SiteFE', f'release/release_mods/{mod}')
         return modfiles
 
     def upgradedb(self, version):
@@ -37,9 +59,12 @@ class DBStarter:
         for key in sortedkeys:
             if key > version:
                 for modfile in modfiles[key]['files']:
-                    with pkg_resources.resource_stream('SiteFE', f'packaging/release_mods/{modfiles[key]["dirname"]}/{modfile}') as fd:
+                    with pkg_resources.resource_stream('SiteFE', f'release/release_mods/{modfiles[key]["dirname"]}/{modfile}') as fd:
                         sql = fd.read().decode('utf-8')
-                    self.db.db.execute(sql)
+                        sql_statements = [line.strip() for line in sql.split('\n') if line.strip()]
+                    for sqlcall in sql_statements:
+                        if sqlcall:
+                            self._makesqlcall(sqlcall)
                 self.db.update("dbversion", {"version": key})
                 version = key
 
@@ -47,7 +72,7 @@ class DBStarter:
         """Start the database creation"""
         self.db.createdb()
         version = self._getversion()
-        if version != runningVersion:
+        if version == self._getversionfloat(runningVersion):
             self.upgradedb(version)
 
 
