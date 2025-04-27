789f450f672f95e1e0a62b003260f396dc0dbef3
Justas Balcas <juztas@gmail.com>
2025-04-27 15:43:25 -0500

    Allow control request line and fields

--- fe/container-build-files/build_files/var/www/wsgi-scripts/gunicorn-start.sh
+++ fe/container-build-files/build_files/var/www/wsgi-scripts/gunicorn-start.sh
@@ -4,22 +4,27 @@ source /etc/environment || true
 set +a
 
 # Default values if not defined in /etc/environment
-WORKERS="${WORKERS:-3}"
-THREADS="${THREADS:-1}"
-MAX_REQUESTS="${MAX_REQUESTS:-800}"
-MAX_REQUESTS_JITTER="${MAX_REQUESTS_JITTER:-100}"
-TIMEOUT="${TIMEOUT:-120}"
-GRACEFUL_TIMEOUT="${GRACEFUL_TIMEOUT:-30}"
-KEEP_ALIVE="${KEEP_ALIVE:-5}"
+GU_WORKERS="${WORKERS:-3}"
+GU_THREADS="${THREADS:-1}"
+GU_MAX_REQUESTS="${MAX_REQUESTS:-800}"
+GU_MAX_REQUESTS_JITTER="${MAX_REQUESTS_JITTER:-100}"
+GU_TIMEOUT="${TIMEOUT:-120}"
+GU_GRACEFUL_TIMEOUT="${GRACEFUL_TIMEOUT:-30}"
+GU_KEEP_ALIVE="${KEEP_ALIVE:-5}"
+GU_LIMIT_REQUEST_LINE="${GU_LIMIT_REQUEST_LINE:-8190}"
+GU_LIMIT_REQUEST_FIELDS="${GU_LIMIT_REQUEST_FIELDS:-32768}"
+GU_LIMIT_REQUEST_BODY="${GU_LIMIT_REQUEST_BODY:-104857600}"
 
 # Start gunicorn
 exec gunicorn sitefe:application \
   -k uvicorn.workers.UvicornWorker \
-  --workers "$WORKERS" \
-  --threads "$THREADS" \
-  --max-requests "$MAX_REQUESTS" \
-  --max-requests-jitter "$MAX_REQUESTS_JITTER" \
-  --timeout "$TIMEOUT" \
-  --graceful-timeout "$GRACEFUL_TIMEOUT" \
-  --keep-alive "$KEEP_ALIVE" \
+  --workers "$GU_WORKERS" \
+  --threads "$GU_THREADS" \
+  --max-requests "$GU_MAX_REQUESTS" \
+  --max-requests-jitter "$GU_MAX_REQUESTS_JITTER" \
+  --timeout "$GU_TIMEOUT" \
+  --graceful-timeout "$GU_GRACEFUL_TIMEOUT" \
+  --keep-alive "$GU_KEEP_ALIVE" \
+  --limit-request-line "$GU_LIMIT_REQUEST_LINE" \
+  --limit-request-fields "$GU_LIMIT_REQUEST_FIELDS" \
   --bind 127.0.0.1:8080
