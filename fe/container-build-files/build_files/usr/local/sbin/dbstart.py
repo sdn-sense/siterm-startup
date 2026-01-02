7e3f85037c9fa0b2280ec44c048bc8d2a87eea9c
Justas Balcas <juztas@gmail.com>
2026-01-02 07:53:57 -0600

    Db start script update

--- fe/container-build-files/build_files/usr/local/sbin/dbstart.py
+++ fe/container-build-files/build_files/usr/local/sbin/dbstart.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Database starter to create and update database"""
 from time import sleep
-import pkg_resources
 import pymysql
 from SiteRMLibs.DBBackend import dbinterface
 from SiteRMLibs import __version__ as runningVersion
@@ -12,20 +11,11 @@ class DBStarter:
     def __init__(self):
         self.db = dbinterface('DBINIT', None, "MAIN")
 
-    def dbready(self):
-        """Check if the database is ready"""
-        try:
-            self.db.db.execute("SELECT 1")
-        except pymysql.OperationalError as ex:
-            print(f"Error executing SQL: {ex}")
-            return False
-        return True
-
     def dboptimize(self):
         """Optimize the database"""
         print("Optimizing database")
         try:
-            out = self.db.db.execute_get("""SELECT CONCAT('CREATE TABLE ', table_name, '_new LIKE ', table_name, '; ',
+            out = self.db.executeRaw("""SELECT CONCAT('CREATE TABLE ', table_name, '_new LIKE ', table_name, '; ',
                                                           'INSERT INTO ', table_name, '_new SELECT * FROM ', table_name, '; ',
                                                           'RENAME TABLE ', table_name, ' TO ', table_name, '_old, ',
                                                                            table_name, '_new TO ', table_name, '; ',
@@ -33,11 +23,12 @@ class DBStarter:
                                                          ) AS migration_commands
                                             FROM information_schema.tables
                                             WHERE table_schema = 'sitefe';""")
-            for row in out[2]:
-                print("Executing SQL Command:", row[0])
-                for item in row[0].split(';'):
+            for row in out:
+                cmd = row[0]
+                print("Executing SQL Command:", cmd)
+                for item in cmd.split(';'):
                     if item.strip():
-                        self.db.db.execute(item.strip())
+                        self.db.executeRaw(item.strip())
         except pymysql.OperationalError as ex:
             print(f"Error executing SQL: {ex}")
             return False
@@ -62,7 +53,7 @@ class DBStarter:
 
     def _makesqlcall(self, sqlcall):
         try:
-            self.db.db.execute(sqlcall)
+            self.db.executeRaw(sqlcall)
         except pymysql.ProgrammingError as ex:
             print(f"Error executing SQL: {ex}")
             raise
@@ -70,56 +61,14 @@ class DBStarter:
             print(f"Error executing SQL: {ex}")
             raise
 
-
-    def _getversion(self):
-        version = self.db.get("dbversion", limit=1)
-        # If no version is found, write the current version
-        if not version:
-            vval = self._getversionfloat(runningVersion)
-            self._insupdversion(vval)
-            return vval
-        return self._getversionfloat(version[0]["version"])
-
-    def _getAllModFiles(self):
-        """Get all the modification files"""
-        try:
-            moddir = pkg_resources.resource_listdir('SiteFE', 'release/release_mods')
-        except FileNotFoundError:
-            return {}
-        modfiles = {}
-        for mod in moddir:
-            modv = self._getversionfloat(mod)
-            modfiles.setdefault(modv, {'dirname': mod, 'files': []})
-            modfiles[modv]['files'] = pkg_resources.resource_listdir('SiteFE', f'release/release_mods/{mod}')
-        return modfiles
-
-    def upgradedb(self, version):
-        """Update the database"""
-        modfiles = self._getAllModFiles()
-        sortedkeys = sorted(modfiles.keys())
-        for key in sortedkeys:
-            if key > version:
-                for modfile in modfiles[key]['files']:
-                    with pkg_resources.resource_stream('SiteFE', f'release/release_mods/{modfiles[key]["dirname"]}/{modfile}') as fd:
-                        sql = fd.read().decode('utf-8')
-                        sql_statements = [line.strip() for line in sql.split('\n') if line.strip()]
-                    for sqlcall in sql_statements:
-                        if sqlcall:
-                            self._makesqlcall(sqlcall)
-                self._insupdversion(key)
-                version = key
-
     def start(self):
         """Start the database creation"""
-        while not self.dbready():
+        while not self.db.isDBReady():
             print("Database not ready, waiting for 1 second. See error above. If continous, check the mariadb process.")
             sleep(1)
         self.db.createdb()
-        self.db.upgradedb()
         self.dboptimize()
-        version = self._getversion()
-        if version != self._getversionfloat(runningVersion):
-            self.upgradedb(version)
+        self._insupdversion(runningVersion)
 
 
 if __name__ == "__main__":
